60c80d1e920e76ad7c03e34b7dc38b9c8ee09ab5
steklovaDM <108463196+steklovaDM@users.noreply.github.com>
2022-07-24 20:20:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿bool task19(string? input)
+{
+    if (String.IsNullOrEmpty(input) || input.Length != 5)
+    {
+        Console.WriteLine("Неверное число");
+        return false;
+    }
+
+    for (int i = 0; i < input.Length / 2; i++)
+    {
+        if (input[i] != input[input.Length - 1 - i])
+            return false;
+    }
+    return true;
+}
+
+void task23(ushort n)
+{
+    for (ushort i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i * i * i);
+    }
+}
+
+Console.WriteLine("Введите пятизначное число\n");
+string? input = Console.ReadLine();
+
+bool polindrom = task19(input);
+Console.WriteLine(polindrom);
+
+
+Console.WriteLine("Введите число\n");
+ushort n = Convert.ToUInt16(Console.ReadLine());
+task23(n);
